543b5d6960b50bceae247cdc6a8db4486e2b4c3f
David <darknight4123@gmail.com>
2021-01-31 10:13:22 +0200

    Tweaked the generic listeners storage

--- src/main/java/com/pseudonova/saverod/models/Ability.java
+++ src/main/java/com/pseudonova/saverod/models/Ability.java
@@ -10,7 +10,7 @@ import java.util.function.Consumer;
 public abstract class Ability {
     private final Rod rod;
     private final String name;
-    private final Map<Class<? extends Event>, Consumer> supportedEvents = new HashMap<>();
+    private final Map<Class<? extends Event>, Consumer<Event>> supportedEvents = new HashMap<>();
 
     public Ability(Rod rod, String name) {
         this.rod = rod;
@@ -24,18 +24,16 @@ public abstract class Ability {
     public Rod getRod() {
         return rod;
     }
-
-    protected <E extends Event> void when(Class<E> eventClass, Consumer<E> eventHandler){
-        this.supportedEvents.put(eventClass, eventHandler);
+    
+    @SuppressWarnings("unchecked") //safe unchecked cast
+    protected <E extends Event> void when(Class<E> eventClass, Consumer<E> handler) {
+        this.supportedEvents.put(eventClass, (Consumer<Event>) handler);
     }
 
-    public boolean isSupportedEvent(Event event){
+    public boolean isSupportedEvent(Event event) {
         return this.supportedEvents.containsKey(event.getClass());
     }
-
-    @SuppressWarnings("unchecked")
     public void activateWithin(Event event) {
-
         this.supportedEvents.get(event.getClass()).accept(event);
     }
 
